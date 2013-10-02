e4c2fba50bcb793fa2985e5557d1ed206fde5642
Leandro <perrotti_leandro@hotmail.com>
2013-10-02 12:09:42 -0300

    modificacion en la matriz: magic number

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -8,43 +8,49 @@ public class Mano {
 	private Forma forma;
 
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
 
 	/*
-	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
-	}
+	 * public Mano(final Forma forma) { throw new
+	 * RuntimeException("No implementado aún"); }
 	 */
 
 	public Mano(final Forma forma) {
-		this.forma = forma;
+		this.forma=forma;
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 
 	public Resultado jugarCon(final Mano otra) {
 
+		int piedra=0;
+		int spock=1;
+		int papel=2;
+		int lagarto=3;
+		int tijera=4;
+		
 		Resultado matriz[][]=new Resultado[5][5];
-		matriz[0][0] = matriz[1][1] = matriz[2][2]= matriz[3][3] = matriz[4][4] = Resultado.EMPATA;
-		matriz[0][3] = matriz[0][4] = matriz[1][0]= matriz[1][4] = matriz[2][0] = matriz[2][1] = matriz[3][1] = matriz[3][2] = matriz[4][2] = matriz[4][3] = Resultado.GANA;
-		matriz[0][1] = matriz[0][2] = matriz[1][2]= matriz[1][3] = matriz[2][3] = matriz[2][4] = matriz[3][0] = matriz[3][4] = matriz[4][0] = matriz[4][1] = Resultado.PIERDE;
+		matriz[piedra][piedra]=matriz[spock][spock]=matriz[papel][papel]=matriz[lagarto][lagarto]=matriz[tijera][tijera]=Resultado.EMPATA;
+		matriz[piedra][lagarto]=matriz[piedra][tijera]=matriz[spock][piedra]=matriz[spock][tijera]=matriz[papel][piedra]=matriz[papel][spock]=matriz[lagarto][spock]=matriz[lagarto][papel]=matriz[tijera][papel]=matriz[tijera][lagarto]=Resultado.GANA;
+		matriz[piedra][spock]=matriz[piedra][papel]=matriz[spock][papel]=matriz[spock][lagarto]=matriz[papel][lagarto]=matriz[papel][tijera]=matriz[lagarto][piedra]=matriz[lagarto][tijera]=matriz[tijera][piedra]=matriz[tijera][spock]=Resultado.PIERDE;
 
-        return matriz[this.forma.getValor()][otra.forma.getValor()];
+		return matriz[this.forma.getValor()][otra.forma.getValor()];
 	}
-
 
 	/*
-	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
-	}
+	 * public Resultado jugarCon(final Mano otra) { throw new
+	 * RuntimeException("No implementado aún"); }
 	 */
 
 }
