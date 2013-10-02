3122249a199d5bcee61982bdd0eba0dda1528ffd
Leandro <perrotti_leandro@hotmail.com>
2013-10-02 12:15:19 -0300

    modificacion en la matriz: magic number. borre datos comentados

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,36 +1,12 @@
 package edu.tallerweb.pptls;
 
-/**
- * Representa una de las Manos involucradas en el juego
- */
 public class Mano {
 
 	private Forma forma;
 
-	/**
-	 * Toda Mano debe crearse con una forma dada, que será la que determine su
-	 * condición en el juego.
-	 * 
-	 * @param forma
-	 *            , la Forma que adopta la Mano.
-	 */
-
-	/*
-	 * public Mano(final Forma forma) { throw new
-	 * RuntimeException("No implementado aún"); }
-	 */
-
 	public Mano(final Forma forma) {
 		this.forma=forma;
 	}
-
-	/**
-	 * Evaluará el resultado de la partida según las reglas del juego.
-	 * 
-	 * @param otra
-	 *            , la otra Mano.
-	 * @return un Resultado, de acuerdo al estado del juego.
-	 */
 
 	public Resultado jugarCon(final Mano otra) {
 
@@ -47,10 +23,4 @@ public class Mano {
 
 		return matriz[this.forma.getValor()][otra.forma.getValor()];
 	}
-
-	/*
-	 * public Resultado jugarCon(final Mano otra) { throw new
-	 * RuntimeException("No implementado aún"); }
-	 */
-
 }
